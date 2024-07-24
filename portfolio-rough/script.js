859a9387f965e176af2945c150bddc7accf4a730
Cole Simpson <cole22simpson@gmail.com>
2024-07-24 12:22:24 -0700

    added recs

--- portfolio-rough/script.js
+++ portfolio-rough/script.js
@@ -4,3 +4,21 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyHAIXn1Pfmof-JNcllpG_RQyJdH-z_55st25ePG-fy_KY0HIohzuxasKJnkYTd6GON/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML= ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
